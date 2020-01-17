7ae21f0978a73f820e49ff8d911455e80a8d583c
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-17 20:18:02 +0100

    Added init to xddp, added getAll view from pokemon/

--- xddp/pokemon/views.py
+++ xddp/pokemon/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pokemon
 
 # Create your views here.
 
 def Home(request):
     return HttpResponse("Hi")
+
+def getAll(request):
+    data = Pokemon.objects.all()
+    return HttpResponse(data)
