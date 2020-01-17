a1781f3157bd5288c983eb2c3d43bcd4510938a1
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-17 19:57:25 +0100

    Added models for team and player, updated Pokemon model

--- xddp/authenticate/views.py
+++ xddp/authenticate/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from xddp.pokemon.models import Player
 
 # Create your views here.
 
 def Home(request):
     return HttpResponse("Hi")
+
+def register(request):
+
+    player = Player()
+    player.save()
+
+
+    return HttpResponse("Player registered")
