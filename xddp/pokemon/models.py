72f16e2ce6e84f41dff0c10810175d3039e331fa
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-19 21:37:46 +0100

    Basic method to add Pokes to Player, and get a player's pokes

--- xddp/pokemon/models.py
+++ xddp/pokemon/models.py
@@ -26,6 +26,15 @@ class Player(models.Model):
     def __str__(self):
         return self.username
 
+    # TODO :
+    # Add a proper create method for Player
+    @classmethod
+    def create(cls):
+        player = cls(name="Sylouan")
+        player.save()
+        return player
+
+
 #############################
 # POKEMON AND POKEMON TEAMS #
 #############################
@@ -60,9 +69,14 @@ class Pokemon(models.Model):
     defense = models.IntegerField(null=True)
     hp = models.IntegerField(null=True)
     name = models.CharField(max_length=50,null=True)
+    level = models.IntegerField(null=True)
+    exp = models.IntegerField(null=True)
     id_poke = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
     id_player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
     id_team = models.ForeignKey(PokemonTeam, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name + str(self.id)
 
     @classmethod
     def ImportOne(cls, id):
@@ -88,6 +102,16 @@ class Pokemon(models.Model):
             poke = Pokemon.objects.get(id_poke=id)
         return poke
 
+
+    #TODO:
+    #Add a proper create method for Pokemon
+    @classmethod
+    def create(cls):
+        player = Player.objects.get(id=1)
+        pokeOne = Pokemon.objects.get(id=1)
+        newpoke = cls(name="Pokemon", id_player=player, id_poke=pokeOne)
+        newpoke.save()
+        return newpoke
 
 
     @classmethod
--- xddp/pokemon/views.py
+++ xddp/pokemon/views.py
@@ -1,9 +1,14 @@
 import json
 
 from django.http import HttpResponse
-from .models import Pokemon
+from .models import Pokemon, Player
 
 from django.core import serializers
+
+###################################################
+#TODO:                                            #
+#Stop being a lazy fuck and do some proper methods#
+###################################################
 
 
 def Home(request):
@@ -12,6 +17,31 @@ def Home(request):
     """
     return HttpResponse("Hi")
 
+def addPlayer(request):
+    Player.create()
+    return HttpResponse("Player added")
+
+def getPlayers(request):
+    list = Player.objects.all()
+    json = serializers.serialize('json', list)
+
+    return HttpResponse(json, content_type="application/json")
+
+def getOwnPokemon(request):
+    #TODO:
+    #Get several pokes and serialize them into a nice json
+    player = Player.objects.get(id=1)
+    pokes = Pokemon.objects.get(id_player=player)
+    #json = serializers.serialize('json', pokes)
+
+    return HttpResponse(pokes)
+
+def addOneRandom(request):
+    """
+    :param playerId: Id of the player the Poke should be added
+    """
+    Pokemon.create()
+    return HttpResponse("PokeAdded")
 
 def getAll(request):
     """
