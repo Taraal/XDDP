20eb84f6e79a96f86fad754a0111ea186e8f9423
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-19 21:37:46 +0100

    Basic method to add Pokes to Player, and get a player's pokes

--- xddp/pokemon/urls.py
+++ xddp/pokemon/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
     path('all/', views.getAll, name='getAll'),
 
     #ATTENTION A NE PAS FLOOD VOTRE BDD#
-    path('import/', views.importAll, name='importAll')
+    path('import/', views.importAll, name='importAll'),
     ####################################
+
+    path('add/', views.addOneRandom, name='addOneRandom'),
+
+    #URLs for testing purposes
+    path('addplayer/', views.addPlayer, name='addPlayer'),
+    path('getplayers/', views.getPlayers, name='getPlayers'),
+    path('getownpokes/', views.getOwnPokemon, name='getOwnPokes'),
 
 ]
