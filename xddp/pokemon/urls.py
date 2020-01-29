4d5d733c403b003868f1872b31a6863ac0cb68eb
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-29 16:12:43 +0100

    Added migrations and early templating

--- xddp/pokemon/urls.py
+++ xddp/pokemon/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='Home'),
     path('all/', views.getAll, name='getAll'),
 
     #ATTENTION A NE PAS FLOOD VOTRE BDD#
-    path('import/', views.importAll, name='importAll'),
+    #path('import/', views.importAll, name='importAll'),
     ####################################
 
     path('add/', views.addOneRandom, name='addOneRandom'),
