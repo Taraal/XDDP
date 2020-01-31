7141ec6851e1c51240e631e228cc203659da41a1
T4raal@gmail.com <Vw9W42YXRnMe>
2020-01-31 11:09:21 +0100

    Added hashing and authentication

--- xddp/authenticate/urls.py
+++ xddp/authenticate/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='Home'),
+    path('', views.authenticate, name='authenticate'),
 ]
--- xddp/authenticate/views.py
+++ xddp/authenticate/views.py
@@ -1,15 +1,45 @@
+import hashlib, binascii, os
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from pokemon.models import Player
 
 # Create your views here.
 
-def Home(request):
-    return HttpResponse("Hi")
+def insertUser(request):
+    try:
+        prenom = request.POST.get("prenom", "")
+        nom = request.POST.get("nom", "")
+        email = request.POST.get('email', "")
+        username = request.POST.get("username", "")
+        password = hashPass(request.POST.get("password"))
+        user_instance = Player.create(prenom, nom, username, email, password)
+        user_instance.save()
+    except Exception as e:
+        return HttpResponse(e)
 
-def register(request):
+    return HttpResponse(True)
 
-    player = Player()
-    player.save()
+def hashPass(password):
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
 
-    return HttpResponse("Player registered")
+    return (salt + pwdhash).decode('ascii')
+
+def authenticate(request):
+    """
+    Checks the hash of the input password
+    :param username
+    :param password
+    :return:
+    """
+    username = request.POST.get("username", "")
+    providedPassword = request.POST.get("password", "")
+    storedPassword = Player.objects.filter(username=username).values_list("password", flat=True)[0]
+    salt = storedPassword[:64]
+    storedPassword = storedPassword[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', providedPassword.encode('utf-8'), salt.encode('ascii'), 100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == storedPassword
