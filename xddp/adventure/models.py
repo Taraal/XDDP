8b88143d538ef2dac4aedeedf294b39b0ecc52b5
T4raal@gmail.com <Vw9W42YXRnMe>
2020-02-07 13:25:49 +0100

    Added early zones

--- xddp/adventure/models.py
+++ xddp/adventure/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from pokemon.models import Pokemon
+
+class Zone(models.Model):
+
+    name = models.TextField(null=True)
+    min_level = models.IntegerField(null=True)
+    max_level = models.IntegerField(null=True, default=(min_level + 10))
+    allowed_pokemon = models.ManyToManyField(Pokemon)
+
+    def __str__(self):
+        return self.name
+
